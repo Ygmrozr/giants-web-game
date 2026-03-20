18df4f96c837ebc47e6425b12d61162c776c6a24
Ygmrozr <e.yagmur.ozer.2002@gmail.com>
2026-03-20 23:50:10 +0300

    #7 update game canvas engine feat: game mechanics implemented (movement, titans, grapple, gas system)

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,48 +1,29 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose"
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    avatar: {
-      type: String,
-      default: "/images/avatars/default.png"
-    },
-    title: {
-      type: String,
-      default: "Acemi Asker"
-    },
-    maxScore: {
-      type: Number,
-      default: 0
-    },
-    totalGames: {
-      type: Number,
-      default: 0
-    },
-    totalKills: {
-      type: Number,
-      default: 0
-    },
-    totalItems: {
-      type: Number,
-      default: 0
-    }
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
 
-module.exports = mongoose.model("User", userSchema);
+username:{
+type:String,
+required:true,
+unique:true
+},
+
+email:{
+type:String,
+required:true,
+unique:true
+},
+
+password:{
+type:String,
+required:true
+},
+
+verified:{
+type:Boolean,
+default:false
+}
+
+})
+
+export default mongoose.model("User",userSchema)
